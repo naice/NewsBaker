e2785f5b274231fb29e05b6b0b2e942c382f655d
Jens Marchewka <jens.marchewka@protel.net>
2019-03-17 23:17:33 +0100

    Added ignored files.

---
+++ _ncoded.NetStandard/Log/ConsoleLogger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ncoded.NetStandard.Log
+{
+    public class ConsoleLogger : ILogger
+    {
+        public void Error(string msg) => Write("ERR", msg);
+        public void Exception(Exception ex) => Write("EXC", $"Type: {ex.GetType().FullName} Exception: {ex.ToString()}");
+        public void Info(string msg) =>Write("INF", msg);
+        public void Warn(string msg) => Write("WAR", msg);
+
+        private void Write(string type, string msg)
+        {
+            Console.WriteLine($"[{type}]: {msg}");
+        }
+    }
+}
---
+++ _ncoded.NetStandard/Log/ILogger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ncoded.NetStandard.Log
+{
+    public interface ILogger
+    {
+        void Info(string msg);
+        void Warn(string msg);
+        void Error(string msg);
+        void Exception(Exception ex);
+    }
+}
